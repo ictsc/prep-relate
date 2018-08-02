7be68019b3f4b25646bc8ba8af8b51b5964c0f94
tar-xzvff <jv120228m@gmail.com>
2018-08-02 14:51:41 +0900

    Update app version

--- docker_build.sh
+++ docker_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build -t relate-webui:20180802003 -f docker/relate_webui/Dockerfile .
+docker build -t relate-webui:20180802004 -f docker/relate_webui/Dockerfile .
 
 docker build -t relate-staticfiles-webserver:20180524 -f docker/relate_staticfiles_webserver/Dockerfile .
